d6296df22b0f5262c0c27d707f80206d314e227f
🇩🇪🇺🇦 Erding | Deutschland - Together 🇺🇦🇩🇪 <deutschland.together@gmail.com>
2025-08-06 00:57:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/deutschlandtogether.github.io/',
   plugins: [react()],
   resolve: {
     alias: {
